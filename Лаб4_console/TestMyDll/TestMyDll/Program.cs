a306fe6a21805d763bb65e55101d11524d69434e
kulikovjenek <kulikovjenek@gmail.com>
2025-03-26 18:26:52 +0300

    Laba in progress...

---
+++ Лаб4_console/TestMyDll/TestMyDll/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KulikovLibrary;
+
+namespace TestMyDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = matOperations.Add(20, 13);
+            Console.WriteLine("Результат сложения: " + sum);
+
+            int minus = matOperations.Substract(20, 13);
+            Console.WriteLine("Результат вычитания: " + minus);
+
+            int mul = matOperations.Multi(20, 13);
+            Console.WriteLine("Результат умножения: " + mul);
+
+            try
+            {
+                double divisionByZero = matOperations.Divide(20, 13);
+                Console.WriteLine("Деление чисел: " + divisionByZero);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+
+
+            try
+            {
+                double divisionByZero = matOperations.Divide(20, 0);
+                Console.WriteLine("Деление чисел: " + divisionByZero); 
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Лаб4_console/TestMyDll/TestMyDll/TessingClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using KulikovLibrary;
+
+
+namespace TestMyDll
+{
+    [TestFixture]
+    class TessingClass
+    {
+        [Test]
+        public void TestCalsKulikov()
+        {
+            ClassicAssert.AreEqual(4, matOperations.Add(2, 2));
+            ClassicAssert.AreEqual(4, matOperations.Substract(6, 2));
+            ClassicAssert.AreEqual(4, matOperations.Multi(2, 2));
+            ClassicAssert.AreEqual(4, matOperations.Divide(8, 2));
+            ClassicAssert.AreEqual(4, matOperations.Divide(80, 20));
+
+        }
+    }
+}
